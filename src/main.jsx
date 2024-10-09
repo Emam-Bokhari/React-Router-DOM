71775de4452073595c14368b133401dbc2c88459
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-09 10:22:04 +0600

    feat(main): add ErrorPage in errorElement  to handole routing errors

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./RootLayout";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout/>,
-    
+    errorElement:<ErrorPage/>
   }
 ]);
 
