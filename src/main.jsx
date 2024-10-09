81f860668fd3219aa010168c5fd04f93f2d4dcf0
Moshfiqur Rahman <studentemam@gmail.com>
2024-10-09 09:13:57 +0600

    refactor(main): replace placeholder div with RootLayout component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./RootLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World!</div>,
+    element: <RootLayout/>,
   }
 ]);
 
